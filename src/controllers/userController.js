94a50a0e51f249734e8e17759ea9fd5136099bea
DanielBorbafs <dev.danielborba@gmail.com>
2025-03-30 21:03:43 -0300

    rota de get configurada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,45 @@
-import Funcionario from "../models/userModel.js";
+import Funcionario from '../models/userModel.js';
 
-// Exemplo de rota para criar um novo funcionário
+// isso aqui cria o o template do funcionário.
 export const createFuncionario = async (req, res) => {
-    try {
-        const { nome, departamento, salario, idade, data_contrato } = req.body;
+  try {
+    const { nome, departamento, salario, idade, data_contrato } = req.body;
 
-        const novoFuncionario = await Funcionario.create({
-            nome,
-            departamento,
-            salario,
-            idade,
-            data_contrato
-        });
+    const novoFuncionario = await Funcionario.create({
+      nome,
+      departamento,
+      salario,
+      idade,
+      data_contrato,
+    });
 
-        return res.status(201).json(novoFuncionario);
-    } catch (err) {
-        console.error('Erro ao criar funcionário:', err);
-        return res.status(500).json({ error: 'Erro interno do servidor' });
+    return res.status(201).json({
+      message: 'Funcionário criado com sucesso',
+      funcionario: novoFuncionario,
+    });
+  } catch (err) {
+    console.error('Erro ao criar funcionário:', err);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+};
+
+export const searchFuncionarios = async (req, res) => {
+  try {
+    // Buscando todos os funcionários no banco de dados
+    const funcionarios = await Funcionario.findAll();
+
+    // Se não houver funcionários, retornamos uma resposta vazia
+    if (funcionarios.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'Nenhum funcionário encontrado.' });
     }
+
+    // Retorna os funcionários encontrados como JSON
+    return res.json(funcionarios);
+  } catch (error) {
+    // Em caso de erro, retorna status 500 e a mensagem de erro
+    console.error(error); // Logando o erro no servidor para debugar
+    res.status(500).json({ error: 'Erro ao buscar funcionários' });
+  }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,15 @@
-import express from "express";
-import { createFuncionario } from "../controllers/userController.js";
+import express from 'express';
+import {
+  createFuncionario,
+  searchFuncionarios,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 // Rota para criar um novo funcionário
-router.post("/funcionarios", createFuncionario);
+router.post('/funcionarios', createFuncionario);
+
+//Rota para trazer todos os funcionários
+router.get('/funcionarios', searchFuncionarios);
 
 export default router;
