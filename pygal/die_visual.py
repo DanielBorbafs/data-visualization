774677b3ca9c281bb413633728cbd97a4fca397d
DanielBorbafs <dev.danielborba@gmail.com>
2025-07-29 10:32:56 -0300

    lançando dois dados ao mesmo tempo

--- pygal/die_visual.py
+++ pygal/die_visual.py
@@ -2,18 +2,20 @@ from die import Die
 import pygal
 
 # Cria um D6
-die = Die()
+die1= Die()
+die2= Die()
 
 # Faz alguns lançamentos e armazena os resultados em uma lista 
 results = []
 
 for roll_num in range(1000):
-    result = die.roll()
+    result = die1.roll() + die2.roll()
     results.append(result)
 
 # Analisa os resultados
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -21,10 +23,10 @@ print(frequencies)
 
 # Visualizando os dados 
 hist = pygal.Bar()
-hist.title = "Resultados de um dado de 6 lados lançado mil vezes"
-hist.x_labels = ['1','2','3', '4', '5', '6'] 
+hist.title = "Resultados de dois dados de 6 lados lançado mil vezes"
+hist.x_labels = ['2','3', '4', '5', '6', '7', '8', '9', '10','11', '12'] 
 hist.x_title = 'Resultado'
 hist.y_tytle = 'Frequencia'
 
-hist.add('D6', frequencies)
+hist.add('D6 + D6', frequencies)
 hist.render_to_file('die_visual.svg')
